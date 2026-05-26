dda22adbce5199ca6e876ea05b8488ae0a22d154
69ScoresThatsME <6833180721@student.chula.ac.th>
2026-05-26 14:59:23 +0700

    add Help page

---
+++ frontend/src/app/help/page.tsx
@@ -0,0 +1,11 @@
+import AllTask from "@/components/TaskDisplayer/AllTask";
+import UrgentTask from "@/components/TaskDisplayer/UrgentTask";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col md:flex-row justify-between w-full md:h-full p-16 pt-8 gap-12">
+      temp
+    </div>
+  );
+}
